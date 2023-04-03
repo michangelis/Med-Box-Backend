71426bf5f0b70feb66aa06ac56014e3aaa1e925f
michangelis <92758264+michangelis@users.noreply.github.com>
2023-04-03 22:28:52 +0300

    Add files via upload
    initial commit for the Backend for Med-Box project

---
+++ MED_Box_Backend/shop/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Customer(models.Model):
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold'),
+    ]
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True)
+    membership = models.CharField(max_length=255)
+    models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_GOLD)
+
+
+class Order(models.Model):
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
+    PAYMENT_STATUS_CHOICES = [
+        (PAYMENT_STATUS_PENDING, 'Pending'),
+        (PAYMENT_STATUS_COMPLETE, 'Complete'),
+        (PAYMENT_STATUS_FAILED, 'Failed'),
+    ]
+
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+
+
+class Address(models.Model):
+    city = models.CharField(max_length=255)
+    street = models.CharField(max_length=255)
+    number = models.IntegerField(max_length=3)
+    zip_code = models.IntegerField(max_length=6)
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
