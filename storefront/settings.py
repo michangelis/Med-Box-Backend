7fcb059a07320d22d0bea88eab4d3be07abf9c92
mican <mican@netcompany.com>
2024-11-19 21:10:27 +0200

    organise repo

--- storefront/settings.py
+++ storefront/settings.py
@@ -38,12 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'playground',
     'debug_toolbar',
-    'store',
-    'store_custom',
-    'tags',
-    'likes',
     'medb'
 ]
 
--- storefront/urls.py
+++ storefront/urls.py
@@ -22,8 +22,6 @@ admin.site.index_title = 'Admin'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('playground/', include('playground.urls')),
-    path('store/', include('store.urls')),
     path('medb/', include('medb.urls')),
     path('__debug__/', include(debug_toolbar.urls)),
 
